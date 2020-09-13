1368ffc014b295c2d10ced5d1cf8a902abd6ffd6
Sharon <srcbun@gmail.com>
2020-09-13 18:09:38 -0400

    force image resize resize before storing

--- src/utilities/cropImage.js
+++ src/utilities/cropImage.js
@@ -7,31 +7,24 @@ const createImage = (url) =>
 		image.src = url
 	})
 
-export default async function getCroppedImg(imageSrc, pixelCrop) {
+export default async function getCroppedImg(imageSrc, crop) {
 	const image = await createImage(imageSrc)
 	const canvas = document.createElement('canvas')
 	const ctx = canvas.getContext('2d')
-
-	const maxSize = Math.max(image.width, image.height)
-	const safeArea = 2 * ((maxSize / 2) * Math.sqrt(2))
-
-	canvas.width = safeArea
-	canvas.height = safeArea
+	
+	canvas.width = 150
+	canvas.height = 150
 
 	ctx.drawImage(
 		image,
-		safeArea / 2 - image.width * 0.5,
-		safeArea / 2 - image.height * 0.5
-	)
-	const data = ctx.getImageData(0, 0, safeArea, safeArea)
-
-	canvas.width = pixelCrop.width
-	canvas.height = pixelCrop.height
-
-	ctx.putImageData(
-		data,
-		Math.round(0 - safeArea / 2 + image.width * 0.5 - pixelCrop.x),
-		Math.round(0 - safeArea / 2 + image.height * 0.5 - pixelCrop.y)
+		crop.x,
+		crop.y,
+		crop.width,
+		crop.height,
+		0,
+		0,
+		canvas.width,
+		canvas.height
 	)
 
 	return new Promise((resolve) => {
