96f8f637f6de8d77f6bff3a590144c7e211e89da
Sharon <srcbun@gmail.com>
2020-09-17 12:34:41 -0400

    change default avatar to be bigger

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UserAvatar = ({ src, username, isCurrentUser }) => {
 	const userProfileName = isCurrentUser ? 'Your' : `${username}'s`
-	const avatar = !src ? 'https://i.imgur.com/tqsfDKG.png' : src
+	const avatar = !src ? 'https://i.imgur.com/OaN8mFD.png' : src
 
 	return (
 		<div className='display-pic__container'>
