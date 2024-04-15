facd99b53b3e0ee9a4be35e4fb424331334a7cf4
Sharon <sharon.gomez@casebook.net>
2024-04-15 16:36:04 -0400

    fix env vars

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -2,18 +2,9 @@ import * as firebase from 'firebase/app'
 import 'firebase/database'
 import 'firebase/storage'
 import 'firebase/auth'
+import config from './config'
 
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
-}
-
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(config)
 
 const database = firebase.database()
 const storage = firebase.storage()
